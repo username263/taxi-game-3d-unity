c18df3e89772521c1a856ea48b3053e75d1b5432
DESKTOP-QTQJQ71\dhkim <dhkim23456@gmail.com>
2023-11-20 18:06:16 +0900

    자동차 구현 InputSystem 추가 자동차 가속/감속 구현

--- taxi-game-3d-client/Assets/_TaxiGame/Scripts/Car.cs
+++ taxi-game-3d-client/Assets/_TaxiGame/Scripts/Car.cs
@@ -1,25 +1,72 @@
 using PathCreation;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-namespace TexiGame3D
+namespace TaxiGame3D
 {
-    public class Car : MonoBehaviour
+    public class Car : MonoBehaviour, InputControls.IPlayerActions
     {
         [SerializeField]
         PathCreator pathCreator;
+        
+        /// <summary>
+        /// 가속도
+        /// </summary>
+        [SerializeField]
+        float acceleration = 1f;
+        /// <summary>
+        /// 최고 속도
+        /// </summary>
+        [SerializeField]
+        float maxSpeed = 5f;
+        /// <summary>
+        /// 제동력
+        /// </summary>
+        [SerializeField]
+        float brakeForce = 1f;
+
+        bool isPressing = false;
+        float speed = 1f;
         float movement = 0f;
-        float speed = 5f;
+
+        InputControls inputControls;
+
 
         void Start()
         {
             transform.position = pathCreator.path.GetPointAtDistance(movement, EndOfPathInstruction.Stop);
         }
 
+        void OnEnable()
+        {
+            if (inputControls == null)
+                inputControls = new();
+            inputControls.Player.SetCallbacks(this);
+            inputControls.Player.Enable();
+        }
+
+        void OnDisable()
+        {
+            inputControls?.Player.Disable();
+        }
+
         void Update()
         {
-            movement += Time.deltaTime * speed;
+            var delta = Time.deltaTime;
+            
+            if (isPressing)
+                speed = Mathf.Min(speed + delta * acceleration, maxSpeed);
+            else
+                speed = Mathf.Max(speed - delta * brakeForce, 1f);
+
+            movement += delta * speed;
             transform.position = pathCreator.path.GetPointAtDistance(movement, EndOfPathInstruction.Stop);
             transform.rotation = pathCreator.path.GetRotationAtDistance(movement, EndOfPathInstruction.Stop);
+        }
+
+        public void OnAccelerate(InputAction.CallbackContext context)
+        {
+            isPressing = context.ReadValue<float>() != 0f;
         }
     }
 }
