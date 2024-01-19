6ba105d696467098070c47aad06877e0cdb89da6
DESKTOP-QTQJQ71\dhkim <dhkim23456@gmail.com>
2024-01-19 18:36:50 +0900

    재화 수집 구현 중

--- taxi-game-3d-client/Assets/_TaxiGame/Scripts/Game/UI/ReadyViewUI.cs
+++ taxi-game-3d-client/Assets/_TaxiGame/Scripts/Game/UI/ReadyViewUI.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
@@ -16,6 +17,12 @@ namespace TaxiGame3D
         Button carListButton;
         [SerializeField]
         Button attendanceButton;
+        [SerializeField]
+        Image collectProgress;
+        [SerializeField]
+        TMP_Text collectAmountText;
+        [SerializeField]
+        Button collectButton;
 
         void Awake()
         {
@@ -38,11 +45,36 @@ namespace TaxiGame3D
             {
                 GameUI.Instance.ShowDailyRewardList();
             });
+            collectButton.onClick.AddListener(() =>
+            {
+                var userService = ClientManager.Instance.UserService;
+                var collectMinutes = (DateTime.UtcNow - userService.User.CoinCollectedAtUtc).TotalMinutes;
+                if (collectMinutes < 1.0)
+                    return;
+                userService.CollectCoin();
+                Refresh();
+            });
         }
 
         void OnEnable()
         {
             Refresh();
+        }
+
+        void Update()
+        {
+            var user = ClientManager.Instance.UserService.User;
+            var collectMinutes = (DateTime.UtcNow - user.CoinCollectedAtUtc).TotalMinutes;
+            collectButton.interactable = collectMinutes < 1.0;
+            if (collectMinutes >= user.CurrentStage.MaxCollect)
+            {
+                collectAmountText.text = user.CurrentStage.MaxCollect.ToString();
+                collectProgress.fillAmount = 1f;
+                return;
+            }
+            var collectAmount = Math.Ceiling(collectMinutes);
+            collectAmountText.text = collectAmount.ToString();
+            collectProgress.fillAmount = (float)(collectMinutes - collectAmount);
         }
 
         public void Refresh()
